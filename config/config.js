2304a202de2504b71b84717b6bbcc2e0701523b1
Ricardhad <134259818+Ricardhad@users.noreply.github.com>
2024-05-09 13:56:29 +0700

    create database if not exist

--- config/config.js
+++ config/config.js
@@ -1,4 +1,36 @@
 const Sequelize = require("sequelize");
+const db = new Sequelize("", "root", "", {
+  host: "localhost",
+  port: "3306",
+  dialect: "mysql",
+  logging: false,
+});
+
+// init kalau belum ada db nya
+db.query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'db_project_ws'")
+  .then(([results]) => {
+    if (results.length === 0) {
+      db.query('CREATE DATABASE db_project_ws')
+        .then(() => {
+          console.log('Database created successfully');
+          db.close(); 
+        })
+        .catch(err => {
+          console.error('Error creating database:', err);
+          db.close(); 
+        });
+    } else {
+      console.log('Database already exists');
+      db.close(); 
+    }
+  })
+  .catch(err => {
+    console.error('Error checking if database exists:', err);
+    db.close(); 
+  });
+
+
+
 const sequelize = new Sequelize("db_project_ws", "root", "", {
   host: "localhost",
   port: "3306",
