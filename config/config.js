5b59344547301ec4528aecb00589815bb42d4464
Ricardhad <134259818+Ricardhad@users.noreply.github.com>
2024-05-09 15:02:59 +0700

    benerin migration seeders

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+const path = require("path");
 const db = new Sequelize("", "root", "", {
   host: "localhost",
   port: "3306",
@@ -13,20 +14,20 @@ db.query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME 
       db.query('CREATE DATABASE db_project_ws')
         .then(() => {
           console.log('Database created successfully');
-          db.close(); 
+          db.close();
         })
         .catch(err => {
           console.error('Error creating database:', err);
-          db.close(); 
+          db.close();
         });
     } else {
       console.log('Database already exists');
-      db.close(); 
+      db.close();
     }
   })
   .catch(err => {
     console.error('Error checking if database exists:', err);
-    db.close(); 
+    db.close();
   });
 
 
@@ -38,4 +39,16 @@ const sequelize = new Sequelize("db_project_ws", "root", "", {
   logging: false,
 });
 
-module.exports = sequelize;
+module.exports = {
+  sequelize,
+  'migrations-path': path.resolve('database', 'migrations'),
+
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "db_project_ws",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+
+};
