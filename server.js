23aeab7fada1aada17ae2ff30612ab50d068bdfc
yosua_christian <qian.liem.christian@gmail.com>
2024-05-10 15:01:04 +0700

    y

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 const trackRoutes = require("./routes/trackRoutes");
+const bodyParser = require('body-parser');
 const dotenv = require("dotenv").config();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
