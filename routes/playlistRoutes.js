fbdc6e67af3c547aa379ecd5e28add4f6ca23d6f
yosua_christian <qian.liem.christian@gmail.com>
2024-06-18 00:26:51 +0700

    lupa ngasih authMiddleware

--- routes/playlistRoutes.js
+++ routes/playlistRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const playlistController = require("../app/controllers/playlistController");
-
-router.post("/", playlistController.createPlayList);
-router.post("/track", playlistController.InsertToPlayList);
-router.delete("/:playlist_id", playlistController.deletePlayList);
-router.delete("/:playlist_id/:track_id", playlistController.deleteTrackList);
+const authMiddleware = require("../app/middleware/authMiddleware");
+router.post("/", authMiddleware,playlistController.createPlayList);
+router.post("/track", authMiddleware,playlistController.InsertToPlayList);
+router.delete("/:playlist_id",authMiddleware ,playlistController.deletePlayList);
+router.delete("/:playlist_id/:track_id", authMiddleware,playlistController.deleteTrackList);
 module.exports = router;
