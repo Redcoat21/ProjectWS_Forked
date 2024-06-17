d196b87461f99d9380e659d0b44f80dcfefaebde
Error404 <yogateguh68@gmail.com>
2024-06-17 15:53:03 +0700

    Update trackRoutes.js

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -3,17 +3,21 @@ const router = express.Router();
 const trackController = require("../app/controllers/trackControler");
 const favoriteController = require("../app/controllers/favoriteController");
 const authMiddleware = require("../app/middleware/authMiddleware");
-
+const premiumMiddleware = require("../app/middleware/premiumMiddleware");
 
 router.get("/:id", authMiddleware, trackController.getTrackById);
-router.get("/lyric/:id",authMiddleware, trackController.getLyrics);
-router.get("/chart/:country/:row",authMiddleware, trackController.chartTrack);
+router.get(
+  "/lyric/:id",
+  authMiddleware,
+  premiumMiddleware,
+  trackController.getLyrics
+);
+router.get("/chart/:country/:row", authMiddleware, trackController.chartTrack);
 
 //rey
 router.post("/favorite", authMiddleware, favoriteController.likeMusic);
 router.delete("/favorite", authMiddleware, favoriteController.deleteLikeMusic);
 
 router.get("/play/:id", authMiddleware, trackController.play);
-
 
 module.exports = router;
